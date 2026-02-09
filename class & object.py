f8036a81dd30232a9f3177896e85631eb502e0b1
R Dheepan <dheepanr1999@gmail.com>
2026-02-09 09:18:44 +0530

    Update and rename 2 class&object.py to class & object.py

--- class & object.py
+++ class & object.py
@@ -15,15 +15,15 @@ ramesh.drink="yes"
 suresh.drink="no"
 
 print(ramesh.name)
-#print("drink",ramesh.drink)
+print("drink",ramesh.drink)
 
-#print(suresh.name)
-#print("drink",suresh.drink)
+print(suresh.name)
+print("drink",suresh.drink)
 
 ramesh.party()
 suresh.beach()
 
-#print("\n")
+print("\n")
 
 class laptop:
     price=0
@@ -44,15 +44,16 @@ lenovo.price=60000
 lenovo.processor="i9"
 lenovo.ram="32GB"
 
-#print("HP laptop price:", hp.price)
-#print("hp processor:", hp.processor)
-#print("hp ram:", hp.ram)
-#print("\n")
-#print("Dell laptop price:", dell.price)
-#print("dell processor:", dell.processor)
-#print("dell ram:", dell.ram)
-#print("\n")
-#print("Lenovo laptop price:", lenovo.price)
+print("HP laptop price:", hp.price)
+print("hp processor:", hp.processor)
+print("hp ram:", hp.ram)
+print("\n")
+print("Dell laptop price:", dell.price)
+print("dell processor:", dell.processor)
+print("dell ram:", dell.ram)
+print("\n")
+print("Lenovo laptop price:", lenovo.price)
 #print("lenovo processor:", lenovo.processor)
 #print("lenovo ram:", lenovo.ram)
+
 
